6070054658b3c37fd0c0a9fde129ae4e9a8f6de8
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 22:44:49 +0300

    TypeChecking using PropTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   render() {
-        return(
-         <div>
-          <h1>{ this.props.txt} </h1>
-        </div>
-        )
-      }
+        let txt = this.props.txt
+        return <h1>{ txt} </h1>
+              }
+}
+App.propTypes = {
+  txt: PropTypes.string,
+  cat: PropTypes.number.isRequired
 }
 
 
