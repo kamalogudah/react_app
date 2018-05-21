bb3ca14ecaf4956b274b5ed42fbb41760b934813
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 23:47:21 +0300

    Using React Ref to get Reference Specific Component

--- src/App.js
+++ src/App.js
@@ -4,27 +4,26 @@ import PropTypes from 'prop-types';
 class App extends React.Component {
    constructor() {
      super();
-     this.state = {currentEvent: '-----'}
-     this.update = this.update.bind(this)
-
+     this.state = {a: ''}
    }
-  update(e) {
-    this.setState({currentEvent: e.type})
-  }
+   update(e) {
+     this.setState({
+       a: this.refs.a.value,
+       b: this.refs.b.value})
+   }
   render() {
     return (
       <div>
-      <textarea
-        onKeyPress={this.update}
-        onCopy={this.update}
-        onCut={this.update}
-        onPaste={this.update}
-        onFocus={this.update}
-        onBlur={this.update}
-        cols="30"
-        rows="10" />
-        <h1>{this.state.currentEvent}</h1>
-     </div>
+        <input
+         ref="a"
+         type="text"
+         onChange={this.update.bind(this)}/>{this.state.a}
+         <hr/>
+         <input
+         ref="b"
+         type="text"
+         onChange={this.update.bind(this)}/>{this.state.b}
+      </div>
     )
   }
 }
