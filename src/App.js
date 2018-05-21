929f80e0a792463ffa75de6d7d59506cebd71ff3
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 22:28:29 +0300

    Using Render to display output

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render() {
-//     return <h1> Hello World</h1>
-//     // return React.createElement('h1', null, 'Hello Eggheads')
-//   }
-// }
-const App = () => <h1> Hello </h1>
+class App extends React.Component {
+  render() {
+        return(
+         <div>
+          <h1> Hello World </h1>
+           <b>Bold</b>
+        </div>
+        )
+     }
+}
+
 
 export default App;
