844a5a5f1694da54240bef5bb3cbced8e36bc84b
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 23:50:10 +0300

    Using React node to get Reference Specific Component

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ class App extends React.Component {
    }
    update(e) {
      this.setState({
-       a: this.refs.a.value,
+       a: this.a.value,
        b: this.refs.b.value})
    }
   render() {
     return (
       <div>
         <input
-         ref="a"
+         ref={node => this.a = node}
          type="text"
          onChange={this.update.bind(this)}/>{this.state.a}
          <hr/>
