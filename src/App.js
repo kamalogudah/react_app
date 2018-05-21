b19b801bd4ae57a74a4984e2981b79f2ab2d189a
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 22:21:32 +0300

    Create a component using class

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
 import React from 'react';
-const App = () => <h1> Hello </h1>
+
+class App extends React.Component {
+  render() {
+    return <h1> Hello World</h1>
+    // return React.createElement('h1', null, 'Hello Eggheads')
+  }
+}
+
 export default App;
